50016cb88313894a02531b97cd8daffc97cbc8fe
Jason5Lee <jason5lee@hotmail.com>
2023-02-18 17:11:10 +0800

    pnpm-esm

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,8 +1,8 @@
 import { assert } from "console";
 
-describe("1984 Reference", () => {
-    it("2 + 2 should not be 5", (done) => {
+describe("2 + 2", () => {
+    it("should not be 5", (done) => {
         assert(2 + 2 != 5);
-        done()
+        done();
     });
 });
